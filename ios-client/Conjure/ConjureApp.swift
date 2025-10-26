fbc8bd8ea679b8c2a98201668e0c126d44131e60
Ant13731 <69334555+Ant13731@users.noreply.github.com>
2025-10-26 12:48:43 -0400

    add base ios client

---
+++ ios-client/Conjure/ConjureApp.swift
@@ -0,0 +1,17 @@
+//
+//  ConjureApp.swift
+//  Conjure
+//
+//  Created by Anthony Hunt on 2025-10-26.
+//
+
+import SwiftUI
+
+@main
+struct ConjureApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ios-client/Conjure/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Conjure
+//
+//  Created by Anthony Hunt on 2025-10-26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
